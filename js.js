3a93140adb8bc434714d6f93822ab066cb7ea515
LAPTOP-D77I3KMJ\toman <tomanom@gmail.com>
2019-11-26 09:26:18 +0200

    remove punctuation mark

--- js.js
+++ js.js
@@ -1,9 +1,8 @@
-var text1 = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-// text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-var text=text1.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+var text = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+var text1 = text.replace(/[.,\/;:]/g,"");
+var text2 = text1.replace(/\s{2,}/g," ");
 
-
-var  array = text.split(" ");
+var  array = text2.split(" ");
 
 var wordCounts = {};
 
